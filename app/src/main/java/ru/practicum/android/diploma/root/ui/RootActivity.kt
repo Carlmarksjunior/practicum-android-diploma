3712412939e968b5ca6583c5e68204143ecf0ea5
Stanpoor <plusiknoisy98@gmail.com>
2026-03-24 11:01:44 +0300

    create bottomnav

--- app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/root/ui/RootActivity.kt
@@ -2,13 +2,30 @@ package ru.practicum.android.diploma.root.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
+import androidx.navigation.ui.setupWithNavController
 import ru.practicum.android.diploma.BuildConfig
 import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ActivityRootBinding
+import ru.practicum.android.diploma.util.getNavController
 
 class RootActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRootBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_root)
+        binding = ActivityRootBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navController = getNavController(R.id.containerView)
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.bottomNavigationView.isVisible =
+                destination.id == R.id.searchFragment ||
+                    destination.id == R.id.favoriteFragment ||
+                    destination.id == R.id.teamFragment
+        }
 
         // Пример использования access token для HeadHunter API
         networkRequestExample(accessToken = BuildConfig.API_ACCESS_TOKEN)
