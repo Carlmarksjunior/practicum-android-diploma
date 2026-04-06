586ebed5c7927ed5441ee30b98b2158ec6c427dc
aza1rat <artemkashityn193@gmail.com>
2026-04-06 18:01:16 +0300

    Верстка экрана настройки фильтрации

--- app/src/main/java/ru/practicum/android/diploma/feature/filter/ui/FilterSettingsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filter/ui/FilterSettingsFragment.kt
@@ -1,11 +1,17 @@
 package ru.practicum.android.diploma.feature.filter.ui
 
 import android.os.Bundle
+import android.text.TextWatcher
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputLayout
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterSettingsBinding
 
@@ -13,6 +19,8 @@ class FilterSettingsFragment : Fragment() {
 
     private var _binging: FragmentFilterSettingsBinding? = null
     private val binding get() = _binging!!
+    private var workplaceTextWatcher: TextWatcher? = null
+    private var industryTextWatcher: TextWatcher? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,10 +33,74 @@ class FilterSettingsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.workplaceTextLayout.setOnClickListener(onWorkplaceClick())
+        binding.workplaceInput.setOnClickListener(onWorkplaceClick())
+        binding.workplaceAction.setOnClickListener(onWorkplaceNavigateOrDelete())
+        binding.industryTextLayout.setOnClickListener(onIndustryClick())
+        binding.industryInput.setOnClickListener(onIndustryClick())
+        binding.industryAction.setOnClickListener(onIndustryNavigateOrDelete())
+        workplaceTextWatcher = binding.workplaceInput.addTextChangedListener(
+            onTextChanged = onTextChanged(binding.workplaceTextLayout, binding.workplaceAction)
+        )
+        industryTextWatcher = binding.industryInput.addTextChangedListener(
+            onTextChanged = onTextChanged(binding.industryTextLayout, binding.industryAction)
+        )
+        binding.workplaceInput.setText("")
+        binding.industryInput.setText("Example")
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        workplaceTextWatcher?.let { binding.workplaceInput.removeTextChangedListener(it) }
+        industryTextWatcher?.let { binding.industryInput.removeTextChangedListener(it) }
+        workplaceTextWatcher = null
+        industryTextWatcher = null
         _binging = null
+    }
+
+    private fun onWorkplaceClick(): View.OnClickListener {
+        return {
+            Toast.makeText(context, "Workplace", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun onWorkplaceNavigateOrDelete(): View.OnClickListener {
+        return {
+            Toast.makeText(context, "Workplace navigate/delete", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun onIndustryClick(): View.OnClickListener {
+        return {
+            Toast.makeText(context, "Industry", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun onIndustryNavigateOrDelete(): View.OnClickListener {
+        return {
+            Toast.makeText(context, "Industry navigate/delete", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun onTextChanged(
+        textLayout: TextInputLayout,
+        actionButton: ImageButton
+    ): (CharSequence?, Int, Int, Int) -> Unit {
+        return { text, _, _, _ ->
+            if (text.isNullOrEmpty()) {
+                textLayout.defaultHintTextColor = ContextCompat.getColorStateList(requireActivity(), R.color.gray)
+                actionButton.setImageResource(R.drawable.ic_arrow_forward)
+            } else {
+                val typedValue = TypedValue()
+                requireActivity().theme.resolveAttribute(
+                    R.attr.colorOnBackgroundPrimary,
+                    typedValue,
+                    true
+                )
+                textLayout.defaultHintTextColor =
+                    ContextCompat.getColorStateList(requireActivity(), typedValue.resourceId)
+                actionButton.setImageResource(R.drawable.ic_close)
+            }
+        }
     }
 }
