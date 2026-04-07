8a7e041ebd6d341c4bc78236b8c8388be5d0c095
JosephPtn <145558577+JosephPtn@users.noreply.github.com>
2026-04-07 11:06:30 +0300

    feature_#89_bugfix

--- app/src/main/java/ru/practicum/android/diploma/feature/filter/domain/impl/FilterSettingsInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/feature/filter/domain/impl/FilterSettingsInteractorImpl.kt
@@ -1,10 +1,9 @@
 package ru.practicum.android.diploma.feature.filter.domain.impl
 
-import ru.practicum.android.diploma.feature.filter.domain.api.FiltersGettingRepository
 import ru.practicum.android.diploma.feature.filter.domain.api.FilterSettingsInteractor
+import ru.practicum.android.diploma.feature.filter.domain.api.FiltersGettingRepository
 import ru.practicum.android.diploma.feature.filter.domain.api.FiltersSavingRepository
 import ru.practicum.android.diploma.feature.filter.domain.model.Filters
-
 class FilterSettingsInteractorImpl(
     private val repository: FiltersGettingRepository,
     private val filtersSavingRepository: FiltersSavingRepository
